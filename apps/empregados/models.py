8d0263f097fca9c4991d80ffce824eee88bce8fe
1000tonaugusto <miltonaugustodesouza@gmail.com>
2023-08-26 20:18:46 -0300

    Relacionamentos do banco de dados

--- apps/empregados/models.py
+++ apps/empregados/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 class Empregado(models.Model):
     nome = models.CharField(max_length=100)
-
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True)
     def __str__(self):
         return self.nome
--- apps/hora_extra/models.py
+++ apps/hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.empregados.models import Empregado
 
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    empregado = models.ForeignKey(Empregado, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.motivo
